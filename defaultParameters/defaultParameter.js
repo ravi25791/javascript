27da7b952b80d03f30555e57d6ee83cde10c2acf
Ravi Kumar <ravi.kumar4rawat@gmail.com>
2025-07-22 11:22:12 +0530

    done

--- defaultParameters/defaultParameter.js
+++ defaultParameters/defaultParameter.js
@@ -1,1 +1,14 @@
-console.log("default parameter")
+function defaultParameter(a, b = 30) {
+    return a + b;
+}
+
+console.log(defaultParameter(20, 80))
+// output - 100
+
+function defaultPar(a, b = 30) {
+    return a + b;
+}
+
+console.log(defaultPar(20))
+
+// output - 50
