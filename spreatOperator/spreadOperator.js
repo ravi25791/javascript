0c1901f6322ee8134dbe897f241e825e1f30815f
Ravi Kumar <ravi.kumar4rawat@gmail.com>
2025-07-27 19:52:49 +0530

    Spread operator

---
+++ spreatOperator/spreadOperator.js
@@ -0,0 +1,12 @@
+console.log("Spread operator")
+
+const num1 = [1, 2, 3, 4]
+const num2 = [5, 6, 7, 8, 9]
+
+const myName = "Ravi"
+
+// const total = num1.concat(num2)
+const totakStr = [...myName]
+const totalNum = [...num1, ...num2]
+console.log("Array : ", totalNum)
+console.log("Str : ", totakStr)
